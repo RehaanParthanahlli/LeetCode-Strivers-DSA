00a158af9ffae230be55eeb3dda244050b292fdc
RehaanParthanahlli <rehaanparthanahalli@gmail.com>
2025-12-09 23:03:11 +0530

    Dawn1

--- Full_One_For_All_DSA.C
+++ Full_One_For_All_DSA.C
@@ -1,1 +1,25 @@
+/*
+**Data Structures and Algorithms**
+*   **Data Structures** (Definition and function: Way to arrange data in memory for efficient usage)
+    *   Examples: Arrays, Linked Lists, Stack, Queue, Graphs, Trees !
+*   **Algorithms** (Definition and function: Sequence of steps to solve a given problem)
+    *   Example: Sorting an Array !
+*   **Programming Language Choice** (C/C++) !
+
+**Terminology and Concepts**
+*   Database !
+*   Data Warehouse (Management of huge amounts of legacy data) !
+*   Big Data !
+
+**Program Memory Management (Memory Layout of Programs)**
+*   RAM/Memory !
+*   Code Segment !
+*   Static and Global Variables !
+*   **Stack** (Normal memory, activation records/stack frames, function calls) !
+*   **Heap** (Dynamic memory allocation) !
+    *   Memory Leak !
+
+**Upcoming Topic**
+*   Time and Space Complexity
+*/
 //Basics 0 - Read it!
